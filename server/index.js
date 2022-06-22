546656d0139a64e203e1a78b43ba8c9cbe3d9dcc
Whitney Harden <wtharden320@gmail.com>
2022-06-21 20:15:55 -0400

    server listening

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 
-const app = express();
 
 const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
 const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const port = process.env.PORT;
 
+const app = express();
 
-console.log(`this is the server`);
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port} 🎶`);
+});
