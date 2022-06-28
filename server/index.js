b796859865ba44389cedd4ea01e432bce051fabb
Whitney Harden <wtharden320@gmail.com>
2022-06-27 20:06:39 -0400

    updated server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config(); // .env file is in the root directory
 const express = require('express');
+const axios = require('axios').default;
 
 const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
 const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
+const redirect_uri = "http://localhost:3000/auth/callback";
 
 let generateRandomString = function (length) {
   let text = '';
@@ -25,14 +27,39 @@ app.get('/auth/login', (req, res) => {
     response_type: "code",
     client_id: spotify_client_id,
     scope: scope,
-    redirect_uri: "http://localhost:3000/auth/callback",
+    redirect_uri: redirect_uri,
     state: state
   })
-  res.redirect(`https://accounts.spotify.com/authorize/?${auth_query_parameters.toString()}`);
+  res.redirect(`https://accounts.spotify.com/authorize/?${auth_query_parameters.toString()}`); // make sure this works using template literals instead of using quotes
 });
 
 app.get('auth/callback', (req, res) => {
+  try {
+    let code = req.body.code;
+    let url = "https://accounts.spotify.com/api/token";
+    console.log(code);
+    console.log(url);
+    let authOptions = {
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code"
+      },
+      headers: {
+        "Authorization": "Basic" + (Buffer.from(spotify_client_id + ":" + spotify_client_secret).toString("base64")),
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      json: true
+    };
 
+    console.log(authOptions);
+
+    axios.post(url, authOptions).then(res => console.log(res)).catch(err => console.error(err));
+    
+  } catch (err) {
+    console.error(err);
+    console.log(`something went wrong`);
+  }
 });
 
 app.listen(port, () => {
